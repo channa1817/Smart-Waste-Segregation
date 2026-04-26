d2ff4eead177e0e41be6a00ef36674498cd28ecc
chenna keshav reddy <119748366+channa1817@users.noreply.github.com>
2026-04-26 23:50:29 +0530

    Added live waste detection Python code

---
+++ ESP32_Code/ESP32_Code/ESP32_Code/ML_Model/live_detection.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+from tensorflow.keras.models import load_model
+
+# Load trained model
+model = load_model("classifyWaste.h5")
+
+# Waste classes
+classes = [
+    "batteries",
+    "clothes",
+    "e-waste",
+    "glass",
+    "light bulbs",
+    "metal",
+    "organic",
+    "paper",
+    "plastic"
+]
+
+# Start webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    # Resize image
+    img = cv2.resize(frame, (224, 224))
+
+    # Normalize
+    img = img / 255.0
+
+    # Expand dimensions
+    img = np.expand_dims(img, axis=0)
+
+    # Prediction
+    prediction = model.predict(img)
+
+    class_index = np.argmax(prediction)
+    label = classes[class_index]
+    confidence = round(np.max(prediction) * 100, 2)
+
+    # Display result
+    cv2.putText(
+        frame,
+        f"{label} ({confidence}%)",
+        (20, 40),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 255, 0),
+        2
+    )
+
+    cv2.imshow("Smart Waste Detection", frame)
+
+    # Press Q to quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
